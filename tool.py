92b649fde1199958b4412806163e59fe56567454
Illidan <illidanrulez@gmail.com>
2021-04-27 09:04:39 +0200

    Bumped hacktools.

--- tool.py
+++ tool.py
@@ -3,7 +3,7 @@ import click
 from hacktools import common, nds
 import game
 
-version = "0.8.0"
+version = "0.8.1"
 data = "VampireData/"
 romfile = data + "vampire.nds"
 rompatch = data + "vampire_patched.nds"
